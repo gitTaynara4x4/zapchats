0f58d46d30a3aa2aced5320dbca26cea9edf911e
Seg Sistemas <vps.segis@gmail.com>
2025-10-15 07:53:49 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,23 +4,25 @@ module.exports = {
   content: [
     './index.html',
 
-    // frontend
+    // tudo do frontend
     './frontend/**/*.html',
     './frontend/**/*.js',
     './frontend/**/*.ts',
-    './frontend/partials/**/*.html',
     './frontend/pages/**/*.html',
+    './frontend/partials/**/*.html',
 
-    // se renderiza pelo backend
+    // se você usa essa pasta também
+    './src/**/*.html',
+    './src/**/*.js',
+
+    // templates renderizados pelo backend (se houver)
     './templates/**/*.html',
   ],
-  // evita escanear lixo
+  theme: { extend: {} },
+  plugins: [],
   safelist: [
-    // classes dark usadas no login:
     'dark:bg-neutral-950','dark:text-neutral-200','dark:bg-neutral-900',
     'dark:border-neutral-700','dark:border-neutral-800','dark:shadow-black/40',
     'dark:text-white','dark:text-gray-400','dark:hidden','dark:block',
   ],
-  theme: { extend: {} },
-  plugins: [],
 };
